d9f4d6e90f8c0a3189df3d0493ffb95d029533a4
Harshit Garg <gargh2120@gmail.com>
2026-02-02 23:48:08 +0530

    Data shows .!

--- src/Data/Data.ts
+++ src/Data/Data.ts
@@ -3,11 +3,12 @@ interface dataprops  {
     id: number,
     itemName: string,
     itemPrice: number
+    qty : number
 }
 
 export const data : dataprops[] = [
-  { id: 1, itemName: "EarBuds", itemPrice: 399 },
-  { id: 2, itemName: "Watch", itemPrice: 799 },
-  { id: 3, itemName: "Bracelette", itemPrice: 1099 },
-  { id: 4, itemName: "Mobile", itemPrice: 10099 },
+  { id: 1, itemName: "EarBuds", itemPrice: 399, qty : 1 },
+  { id: 2, itemName: "Watch", itemPrice: 799 , qty : 1},
+  { id: 3, itemName: "Bracelette", itemPrice: 1099 , qty : 1},
+  { id: 4, itemName: "Mobile", itemPrice: 10099 , qty : 1 },
 ];
