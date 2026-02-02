594515f2f431d7ecf60623a303d468cb6c23d0fb
Harshit Garg <gargh2120@gmail.com>
2026-02-02 23:09:38 +0530

    Fix some error !

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,11 +1,10 @@
 import type { QueueItem } from "../App";
-
+import "../index.css";
 interface CartProps {
-  Queue : QueueItem[]
+  Queue: QueueItem[];
 }
 
-
-function Cart({Queue}: CartProps) {
+function Cart({ Queue }: CartProps) {
   if (Queue.length == 0) {
     return (
       <div className="p-8 flex w-120 items-center rounded-2xl justify-center bg-[#201d1d]">
@@ -17,18 +16,18 @@ function Cart({Queue}: CartProps) {
   }
   return (
     <>
-      {Queue &&
-        Queue.map((item) => (
-          <div className="p-8 flex w-120 items-center rounded-2xl justify-center bg-[#201d1d]">
-            <div className="bg-zinc-900 text-white w-80 p-6 rounded-xl shadow-lg">
-              {/* Title */}
+      <div className="p-8 flex w-120 items-center rounded-2xl justify-center bg-[#201d1d]">
+        <div className="bg-zinc-900 text-white w-80 max-h-90 no-scrollbar overflow-y-scroll  p-6 rounded-xl shadow-lg">
+          {/* Title */}
 
-              <h2 className="text-lg font-semibold mb-4">Shopping Cart</h2>
+          <h2 className="text-lg font-semibold mb-4">Shopping Cart</h2>
 
-              <hr className="border-zinc-700 mb-4" />
+          <hr className="border-zinc-700 mb-4" />
+          {/* Product */}
 
-              {/* Product */}
-              <div className="flex justify-between items-start mb-4">
+          {Queue.map((item) => (
+            <>
+              <div className="flex justify-between items-start  mb-4">
                 <div>
                   <h3 className="text-sm font-medium">{item.name}</h3>
                   <p className="text-blue-400 text-sm font-semibold">
@@ -54,14 +53,14 @@ function Cart({Queue}: CartProps) {
                 <span>Total:</span>
                 <span>{item.name}</span>
               </div>
-
-              {/* Checkout */}
-              <button className="w-full bg-green-600 hover:bg-green-700 py-2 rounded-lg font-medium">
-                Checkout
-              </button>
-            </div>
-          </div>
-        ))}
+            </>
+          ))}
+          {/* Checkout */}
+          <button className="w-full bg-green-600 hover:bg-green-700 py-2 rounded-lg font-medium">
+            Checkout
+          </button>
+        </div>
+      </div>
     </>
   );
 }
