dacbb6ab7f6bd63fb45ac72c1bda1318fae90fb9
Harshit Garg <gargh2120@gmail.com>
2026-02-02 22:48:04 +0530

    Collection of items in a cart  !

---
+++ src/Data/Data.ts
@@ -0,0 +1,13 @@
+
+interface dataprops  {
+    id: number,
+    itemName: string,
+    itemPrice: number
+}
+
+export const data : dataprops[] = [
+  { id: 1, itemName: "EarBuds", itemPrice: 399 },
+  { id: 2, itemName: "Watch", itemPrice: 799 },
+  { id: 3, itemName: "Bracelette", itemPrice: 1099 },
+  { id: 4, itemName: "Mobile", itemPrice: 10099 },
+];
